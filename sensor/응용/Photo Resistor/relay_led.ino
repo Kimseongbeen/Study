2bd8e350a6f1a199ff13e2d0c2914f18f91c93e5
김성빈 <59568377+Kimseongbeen@users.noreply.github.com>
2021-06-06 19:31:51 +0900

    조도센서,Relay를 이용해 백열전구 자동 제어

---
+++ sensor/응용/Photo Resistor/relay_led.ino
@@ -0,0 +1,22 @@
+int S = A0; // Analog Input
+int d1=D1;
+void setup()
+{
+ Serial.begin(115200); // 시리얼 통신 초기화
+ pinMode(d1, OUTPUT);
+ pinMode(S, INPUT);
+}
+
+void loop()
+{
+  int sensor=analogRead(S);
+
+  if(sensor>15){             //센서값이 15 초과일때 NC
+    digitalWrite(d1,LOW);
+}
+  else if(sensor<=15){       //센서값이 15 이하일때 NO 
+    digitalWrite(d1,HIGH);
+  }
+ Serial.println(sensor);
+ delay(1000);
+}
