d9704eea67303c32632bb914554cccf10b969211
flowercat <59568377+Kimseongbeen@users.noreply.github.com>
2024-09-14 20:25:11 +0900

    simple_controller.hpp 수정
    SimpleController 클래스의 헤더 파일과 구현 파일 사이의 불일치 해결

--- ROS Project/ROS2/Project/3. TurtleBot/install ROS2 on Raspberry Pi/bumperbot_ws/src/bumperbot_controller/include/bumperbot_controller/simple_controller.hpp
+++ ROS Project/ROS2/Project/3. TurtleBot/install ROS2 on Raspberry Pi/bumperbot_ws/src/bumperbot_controller/include/bumperbot_controller/simple_controller.hpp
@@ -1,46 +1,43 @@
-#ifndef SIMPLE_CONTROLLER_HPP
-#define SIMPLE_CONTROLLER_HPP
+#ifndef BUMPERBOT_CONTROLLER__SIMPLE_CONTROLLER_HPP_
+#define BUMPERBOT_CONTROLLER__SIMPLE_CONTROLLER_HPP_
 
 #include <rclcpp/rclcpp.hpp>
-#include <geometry_msgs/msg/twist_stamped.hpp>
-#include <Eigen/Core>
 #include <std_msgs/msg/float64_multi_array.hpp>
-#include <sensor_msgs/msg/joint_state.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
 #include <nav_msgs/msg/odometry.hpp>
-#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <tf2_ros/transform_broadcaster.h>
-
+#include <Eigen/Dense>
 
 class SimpleController : public rclcpp::Node
 {
 public:
-    SimpleController(const std::string& name);
+  SimpleController(const std::string& name);
 
 private:
-    void velCallback(const geometry_msgs::msg::TwistStamped &msg);
+  void velCallback(const geometry_msgs::msg::TwistStamped& msg);
+  void updateOdometry();
 
-    void jointCallback(const sensor_msgs::msg::JointState &msg);
+  rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_cmd_pub_;
+  rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
+  rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
 
-    rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
-    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_cmd_pub_;
-    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_sub_;
-    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> transform_broadcaster_;
 
-    // Odometry
-    double wheel_radius_;
-    double wheel_separation_;
-    Eigen::Matrix2d speed_conversion_;
-    double right_wheel_prev_pos_;
-    double left_wheel_prev_pos_;
-    rclcpp::Time prev_time_;
-    nav_msgs::msg::Odometry odom_msg_;
-    double x_;
-    double y_;
-    double theta_;
+  double wheel_radius_;
+  double wheel_separation_;
+  Eigen::Matrix2d speed_conversion_;
 
-    // TF
-    std::unique_ptr<tf2_ros::TransformBroadcaster> transform_broadcaster_;
-    geometry_msgs::msg::TransformStamped transform_stamped_;
+  double x_;
+  double y_;
+  double theta_;
+
+  double current_linear_vel_;
+  double current_angular_vel_;
+
+  rclcpp::Time prev_time_;
+  nav_msgs::msg::Odometry odom_msg_;
+  geometry_msgs::msg::TransformStamped transform_stamped_;
 };
 
-#endif // SIMPLE_CONTROLLER_HPP
+#endif  // BUMPERBOT_CONTROLLER__SIMPLE_CONTROLLER_HPP_
